74d324e50149b7addd135f7aaed4151813dd1cff
Isaac Costa <isaacpersonalacc@gmail.com>
2020-06-21 15:42:03 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace Calculadora
+{
+    partial class mainWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.buttonAlpha1 = new System.Windows.Forms.Button();
+			this.buttonAlpha2 = new System.Windows.Forms.Button();
+			this.buttonAlpha3 = new System.Windows.Forms.Button();
+			this.buttonAlpha4 = new System.Windows.Forms.Button();
+			this.buttonAlpha5 = new System.Windows.Forms.Button();
+			this.buttonAlpha6 = new System.Windows.Forms.Button();
+			this.buttonAlpha7 = new System.Windows.Forms.Button();
+			this.buttonAlpha8 = new System.Windows.Forms.Button();
+			this.buttonAlpha9 = new System.Windows.Forms.Button();
+			this.buttonAlpha0 = new System.Windows.Forms.Button();
+			this.res = new System.Windows.Forms.Label();
+			this.iguals = new System.Windows.Forms.Button();
+			this.calcs = new System.Windows.Forms.Label();
+			this.plus = new System.Windows.Forms.Button();
+			this.minus = new System.Windows.Forms.Button();
+			this.div = new System.Windows.Forms.Button();
+			this.versus = new System.Windows.Forms.Button();
+			this.title = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// buttonAlpha1
+			// 
+			this.buttonAlpha1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+			this.buttonAlpha1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+			this.buttonAlpha1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.buttonAlpha1.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+			this.buttonAlpha1.ForeColor = System.Drawing.Color.White;
+			this.buttonAlpha1.Location = new System.Drawing.Point(175, 352);
+			this.buttonAlpha1.Name = "buttonAlpha1";
+			this.buttonAlpha1.Size = new System.Drawing.Size(75, 75);
+			this.buttonAlpha1.TabIndex = 2;
+			this.buttonAlpha1.Text = "1";
+			this.buttonAlpha1.UseVisualStyleBackColor = false;
+			this.buttonAlpha1.Click += new System.EventHandler(this.numberAlpha1_Click);
+			// 
+			// buttonAlpha2
+			// 
+			this.buttonAlpha2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+			this.buttonAlpha2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+			this.buttonAlpha2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.buttonAlpha2.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+			this.buttonAlpha2.ForeColor = System.Drawing.Color.White;
+			this.buttonAlpha2.Location = new System.Drawing.Point(94, 352);
+			this.buttonAlpha2.Name = "buttonAlpha2";
+			this.buttonAlpha2.Size = new System.Drawing.Size(75, 75);
+			this.buttonAlpha2.TabIndex = 2;
+			this.buttonAlpha2.Text = "2";
+			this.buttonAlpha2.UseVisualStyleBackColor = false;
+			this.buttonAlpha2.Click += new System.EventHandler(this.buttonAlpha2_Click);
+			// 
+			// buttonAlpha3
+			// 
+			this.buttonAlpha3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+			this.buttonAlpha3.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+			this.buttonAlpha3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.buttonAlpha3.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+			this.buttonAlpha3.ForeColor = System.Drawing.Color.White;
+			this.buttonAlpha3.Location = new System.Drawing.Point(13, 352);
+			this.buttonAlpha3.Name = "buttonAlpha3";
+			this.buttonAlpha3.Size = new System.Drawing.Size(75, 75);
+			this.buttonAlpha3.TabIndex = 3;
+			this.buttonAlpha3.Text = "3";
+			this.buttonAlpha3.UseVisualStyleBackColor = false;
+			this.buttonAlpha3.Click += new System.EventHandler(this.buttonAlpha3_Click);
+			// 
+			// buttonAlpha4
+			// 
+			this.buttonAlpha4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+			this.buttonAlpha4.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+			this.buttonAlpha4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.buttonAlpha4.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+			this.buttonAlpha4.ForeColor = System.Drawing.Color.White;
+			this.buttonAlpha4.Location = new System.Drawing.Point(175, 271);
+			this.buttonAlpha4.Name = "buttonAlpha4";
+			this.buttonAlpha4.Size = new System.Drawing.Size(75, 75);
+			this.buttonAlpha4.TabIndex = 4;
+			this.buttonAlpha4.Text = "4";
+			this.buttonAlpha4.UseVisualStyleBackColor = false;
+			this.buttonAlpha4.Click += new System.EventHandler(this.buttonAlpha4_Click);
+			// 
+			// buttonAlpha5
+			// 
+			this.buttonAlpha5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+			this.buttonAlpha5.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+			this.buttonAlpha5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.buttonAlpha5.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+			this.buttonAlpha5.ForeColor = System.Drawing.Color.White;
+			this.buttonAlpha5.Location = new System.Drawing.Point(94, 271);
+			this.buttonAlpha5.Name = "buttonAlpha5";
+			this.buttonAlpha5.Size = new System.Drawing.Size(75, 75);
+			this.buttonAlpha5.TabIndex = 5;
+			this.buttonAlpha5.Text = "5";
+			this.buttonAlpha5.UseVisualStyleBackColor = false;
+			this.buttonAlpha5.Click += new System.EventHandler(this.buttonAlpha5_Click);
+			// 
+			// buttonAlpha6
+			// 
+			this.buttonAlpha6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+			this.buttonAlpha6.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+			this.buttonAlpha6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.buttonAlpha6.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+			this.buttonAlpha6.ForeColor = System.Drawing.Color.White;
+			this.buttonAlpha6.Location = new System.Drawing.Point(13, 272);
+			this.buttonAlpha6.Name = "buttonAlpha6";
+			this.buttonAlpha6.Size = new System.Drawing.Size(75, 75);
+			this.buttonAlpha6.TabIndex = 6;
+			this.buttonAlpha6.Text = "6";
+			this.buttonAlpha6.UseVisualStyleBackColor = false;
+			this.buttonAlpha6.Click += new System.EventHandler(this.buttonAlpha6_Click);
+			// 
+			// buttonAlpha7
+			// 
+			this.buttonAlpha7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+			this.buttonAlpha7.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+			this.buttonAlpha7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.buttonAlpha7.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+			this.buttonAlpha7.ForeColor = System.Drawing.Color.White;
+			this.buttonAlpha7.Location = new System.Drawing.Point(175, 191);
+			this.buttonAlpha7.Name = "buttonAlpha7";
+			this.buttonAlpha7.Size = new System.Drawing.Size(75, 75);
+			this.buttonAlpha7.TabIndex = 7;
+			this.buttonAlpha7.Text = "7";
+			this.buttonAlpha7.UseVisualStyleBackColor = false;
+			this.buttonAlpha7.Click += new System.EventHandler(this.buttonAlpha7_Click);
+			// 
+			// buttonAlpha8
+			// 
+			this.buttonAlpha8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+			this.buttonAlpha8.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+			this.buttonAlpha8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.buttonAlpha8.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+			this.buttonAlpha8.ForeColor = System.Drawing.Color.White;
+			this.buttonAlpha8.Location = new System.Drawing.Point(94, 191);
+			this.buttonAlpha8.Name = "buttonAlpha8";
+			this.buttonAlpha8.Size = new System.Drawing.Size(75, 75);
+			this.buttonAlpha8.TabIndex = 8;
+			this.buttonAlpha8.Text = "8";
+			this.buttonAlpha8.UseVisualStyleBackColor = false;
+			this.buttonAlpha8.Click += new System.EventHandler(this.buttonAlpha8_Click);
+			// 
+			// buttonAlpha9
+			// 
+			this.buttonAlpha9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+			this.buttonAlpha9.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+			this.buttonAlpha9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.buttonAlpha9.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+			this.buttonAlpha9.ForeColor = System.Drawing.Color.White;
+			this.buttonAlpha9.Location = new System.Drawing.Point(13, 191);
+			this.buttonAlpha9.Name = "buttonAlpha9";
+			this.buttonAlpha9.Size = new System.Drawing.Size(75, 75);
+			this.buttonAlpha9.TabIndex = 9;
+			this.buttonAlpha9.Text = "9";
+			this.buttonAlpha9.UseVisualStyleBackColor = false;
+			this.buttonAlpha9.Click += new System.EventHandler(this.buttonAlpha9_Click);
+			// 
+			// buttonAlpha0
+			// 
+			this.buttonAlpha0.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+			this.buttonAlpha0.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+			this.buttonAlpha0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.buttonAlpha0.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+			this.buttonAlpha0.ForeColor = System.Drawing.Color.White;
+			this.buttonAlpha0.Location = new System.Drawing.Point(13, 433);
+			this.buttonAlpha0.Name = "buttonAlpha0";
+			this.buttonAlpha0.Size = new System.Drawing.Size(237, 75);
+			this.buttonAlpha0.TabIndex = 10;
+			this.buttonAlpha0.Text = "0";
+			this.buttonAlpha0.UseVisualStyleBackColor = false;
+			this.buttonAlpha0.Click += new System.EventHandler(this.buttonAlpha0_Click);
+			// 
+			// res
+			// 
+			this.res.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+			this.res.Font = new System.Drawing.Font("HoloLens MDL2 Assets", 50F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Pixel);
+			this.res.ForeColor = System.Drawing.Color.White;
+			this.res.Location = new System.Drawing.Point(12, 32);
+			this.res.Name = "res";
+			this.res.Size = new System.Drawing.Size(316, 67);
+			this.res.TabIndex = 17;
+			this.res.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this.res.UseMnemonic = false;
+			this.res.Click += new System.EventHandler(this.res_Click);
+			// 
+			// iguals
+			// 
+			this.iguals.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.iguals.Location = new System.Drawing.Point(13, 514);
+			this.iguals.Name = "iguals";
+			this.iguals.Size = new System.Drawing.Size(237, 74);
+			this.iguals.TabIndex = 18;
+			this.iguals.Text = "=";
+			this.iguals.UseVisualStyleBackColor = true;
+			this.iguals.Click += new System.EventHandler(this.iguals_Click);
+			// 
+			// calcs
+			// 
+			this.calcs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+			this.calcs.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.calcs.ForeColor = System.Drawing.Color.White;
+			this.calcs.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+			this.calcs.Location = new System.Drawing.Point(12, 99);
+			this.calcs.Name = "calcs";
+			this.calcs.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.calcs.Size = new System.Drawing.Size(316, 77);
+			this.calcs.TabIndex = 19;
+			this.calcs.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this.calcs.Click += new System.EventHandler(this.calcs_Click);
+			// 
+			// plus
+			// 
+			this.plus.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.plus.Location = new System.Drawing.Point(256, 433);
+			this.plus.Name = "plus";
+			this.plus.Size = new System.Drawing.Size(75, 155);
+			this.plus.TabIndex = 20;
+			this.plus.Text = "+";
+			this.plus.UseVisualStyleBackColor = true;
+			this.plus.Click += new System.EventHandler(this.plus_Click);
+			// 
+			// minus
+			// 
+			this.minus.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.minus.Location = new System.Drawing.Point(256, 352);
+			this.minus.Name = "minus";
+			this.minus.Size = new System.Drawing.Size(75, 75);
+			this.minus.TabIndex = 21;
+			this.minus.Text = "-";
+			this.minus.UseVisualStyleBackColor = true;
+			this.minus.Click += new System.EventHandler(this.minus_Click);
+			// 
+			// div
+			// 
+			this.div.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.div.Location = new System.Drawing.Point(257, 272);
+			this.div.Name = "div";
+			this.div.Size = new System.Drawing.Size(75, 74);
+			this.div.TabIndex = 22;
+			this.div.Text = ":";
+			this.div.UseVisualStyleBackColor = true;
+			this.div.Click += new System.EventHandler(this.div_Click);
+			// 
+			// versus
+			// 
+			this.versus.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.versus.Location = new System.Drawing.Point(257, 191);
+			this.versus.Name = "versus";
+			this.versus.Size = new System.Drawing.Size(75, 75);
+			this.versus.TabIndex = 23;
+			this.versus.Text = "x";
+			this.versus.UseVisualStyleBackColor = true;
+			this.versus.Click += new System.EventHandler(this.versus_Click);
+			// 
+			// title
+			// 
+			this.title.BackColor = System.Drawing.Color.Transparent;
+			this.title.Font = new System.Drawing.Font("Segoe MDL2 Assets", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.title.ForeColor = System.Drawing.Color.White;
+			this.title.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.title.Location = new System.Drawing.Point(13, -13);
+			this.title.Name = "title";
+			this.title.Size = new System.Drawing.Size(315, 45);
+			this.title.TabIndex = 24;
+			this.title.Text = "CALCuladora";
+			this.title.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.title.Click += new System.EventHandler(this.title_Click);
+			// 
+			// mainWindow
+			// 
+			this.AllowDrop = true;
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+			this.BackColor = System.Drawing.Color.Black;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.ClientSize = new System.Drawing.Size(340, 604);
+			this.Controls.Add(this.title);
+			this.Controls.Add(this.versus);
+			this.Controls.Add(this.div);
+			this.Controls.Add(this.minus);
+			this.Controls.Add(this.plus);
+			this.Controls.Add(this.calcs);
+			this.Controls.Add(this.iguals);
+			this.Controls.Add(this.res);
+			this.Controls.Add(this.buttonAlpha0);
+			this.Controls.Add(this.buttonAlpha9);
+			this.Controls.Add(this.buttonAlpha8);
+			this.Controls.Add(this.buttonAlpha7);
+			this.Controls.Add(this.buttonAlpha6);
+			this.Controls.Add(this.buttonAlpha5);
+			this.Controls.Add(this.buttonAlpha4);
+			this.Controls.Add(this.buttonAlpha3);
+			this.Controls.Add(this.buttonAlpha2);
+			this.Controls.Add(this.buttonAlpha1);
+			this.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "mainWindow";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+
+        }
+
+		#endregion
+		private System.Windows.Forms.Button buttonAlpha1;
+		private System.Windows.Forms.Button buttonAlpha2;
+		private System.Windows.Forms.Button buttonAlpha3;
+		private System.Windows.Forms.Button buttonAlpha4;
+		private System.Windows.Forms.Button buttonAlpha5;
+		private System.Windows.Forms.Button buttonAlpha6;
+		private System.Windows.Forms.Button buttonAlpha7;
+		private System.Windows.Forms.Button buttonAlpha8;
+		private System.Windows.Forms.Button buttonAlpha9;
+		private System.Windows.Forms.Button buttonAlpha0;
+		private System.Windows.Forms.Label res;
+		private System.Windows.Forms.Button iguals;
+		private System.Windows.Forms.Label calcs;
+		private System.Windows.Forms.Button plus;
+		private System.Windows.Forms.Button minus;
+		private System.Windows.Forms.Button div;
+		private System.Windows.Forms.Button versus;
+		private System.Windows.Forms.Label title;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class mainWindow : Form
+    {
+		// String para armazenar o que o usuario digitar
+		public string userNum;
+
+		// Numeros que recebem os valores digitados depois de convertido
+		private float fNum;
+		private float fNum2;
+
+		// 
+		private int maxSignalToCalc = 1;
+		private bool plusCalc = false;
+		private bool minusCalc = false;
+		private bool versusCalc = false;
+		private bool divCalc = false;
+
+
+		public mainWindow()
+        {
+            InitializeComponent();
+        }
+
+		private void numberAlpha1_Click(object sender, EventArgs e)
+		{
+			userNum += "1";
+			calcs.Text += "1";
+		}
+
+		private void buttonAlpha2_Click(object sender, EventArgs e)
+		{
+			userNum += "2";
+			calcs.Text += "2";
+		}
+
+		private void buttonAlpha3_Click(object sender, EventArgs e)
+		{
+			userNum += "3";
+			calcs.Text += "3";
+		}
+
+		private void buttonAlpha4_Click(object sender, EventArgs e)
+		{
+			userNum += "4";
+			calcs.Text += "4";
+		}
+
+		private void buttonAlpha5_Click(object sender, EventArgs e)
+		{
+			userNum += "5";
+			calcs.Text += "5";
+		}
+
+		private void buttonAlpha6_Click(object sender, EventArgs e)
+		{
+			userNum += "6";
+			calcs.Text += "6";
+		}
+
+		private void buttonAlpha7_Click(object sender, EventArgs e)
+		{
+			userNum += "7";
+			calcs.Text += "7";
+		}
+
+		private void buttonAlpha8_Click(object sender, EventArgs e)
+		{
+			userNum += "8";
+			calcs.Text += "8";
+		}
+
+		private void buttonAlpha9_Click(object sender, EventArgs e)
+		{
+			userNum += "9";
+			calcs.Text += "9";
+		}
+
+		private void buttonAlpha0_Click(object sender, EventArgs e)
+		{
+			userNum += "0";
+			calcs.Text += "0";
+		}
+
+		private void iguals_Click(object sender, EventArgs e)
+		{
+			if(userNum != null)
+			{
+				fNum2 = ConvertToFloat(userNum);
+
+				if (plusCalc)
+				{
+					res.Text = (fNum + fNum2).ToString();
+					plusCalc = false;
+				}
+
+				if (minusCalc)
+				{
+					res.Text = (fNum - fNum2).ToString();
+					minusCalc = false;
+				}
+
+				if (divCalc)
+				{
+					res.Text = (fNum / fNum2).ToString();
+					divCalc = false;
+				}
+
+				if (versusCalc)
+				{
+					res.Text = (fNum * fNum2).ToString();
+					versusCalc = false;
+				}
+
+				calcs.Text = "";
+				userNum = "0";
+
+
+				fNum = 0;
+				fNum2 = 0;
+			}
+			maxSignalToCalc = 1;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			// Na inicialização
+			res.Text = "       ";
+		}
+
+		private void res_Click(object sender, EventArgs e)
+		{
+			res.Text = "       ";
+		}
+
+		private void calcs_Click(object sender, EventArgs e)
+		{
+			calcs.Text = "";
+			userNum = "0";
+		}
+
+		private void plus_Click(object sender, EventArgs e)
+		{
+			if (maxSignalToCalc > 0)
+			{
+				if (userNum != null)
+				{
+					plusCalc = true;
+					fNum = ConvertToFloat(userNum);
+					userNum = "0";
+					calcs.Text += " + ";
+
+					maxSignalToCalc = 0;
+				}
+			}
+		}
+		private void minus_Click(object sender, EventArgs e)
+		{
+			if (maxSignalToCalc > 0)
+			{
+				if (userNum != null)
+				{
+					minusCalc = true;
+					fNum = ConvertToFloat(userNum);
+					userNum = "0";
+					calcs.Text += " - ";
+
+					maxSignalToCalc = 0;
+				}
+			}
+		}
+		private void div_Click(object sender, EventArgs e)
+		{
+			if (maxSignalToCalc > 0)
+			{
+				if (userNum != null)
+				{
+					divCalc = true;
+					fNum = ConvertToFloat(userNum);
+					userNum = "0";
+					calcs.Text += " / ";
+
+					maxSignalToCalc = 0;
+				}
+			}
+		}
+		private void versus_Click(object sender, EventArgs e)
+		{
+			if (maxSignalToCalc > 0)
+			{
+				if (userNum != null)
+				{
+					versusCalc = true;
+					fNum = ConvertToFloat(userNum);
+					userNum = "0";
+					calcs.Text += " x ";
+
+					maxSignalToCalc = 0;
+				}
+			}
+		}
+
+		private void title_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		/// <summary>
+		/// Função que retorna a conversão de uma string em float
+		/// </summary>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		private float ConvertToFloat (string value)
+		{
+			return Convert.ToSingle(value);
+		}
+
+	}
+}
